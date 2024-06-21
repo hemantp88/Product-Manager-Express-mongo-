94e22e8112061746d97bd93cf063c7385dd920b3
hemantp88 <panchalhemant030700@gmail.com>
2024-06-21 20:03:56 +0530

    mongoose setup with first get route

--- index.js
+++ index.js
@@ -2,10 +2,29 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const mongoose = require('mongoose');
+const { error } = require('console');
+mongoose.connect('mongodb://127.0.0.1:27017/myapp')
+    .then(() => {
+        console.log("Mongo  Connection Open");
+    })
+    .catch(error => {
+        console.log("Oh No Mongo connection error !!!");
+        console.log(error);
+    })
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
+app.get('/dogs', (req, res) => {
+    res.send("wood");
+})
+
+
 
 
 app.listen(3000, () => {
     console.log("Lintening on port 3000 !")
 })
+
