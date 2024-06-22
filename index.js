841fb5c569986dc98b8a3f05ee4eaf766ff0faff
hemantp88 <panchalhemant030700@gmail.com>
2024-06-22 12:11:55 +0530

    filtering  product by category

--- index.js
+++ index.js
@@ -27,15 +27,24 @@ app.set('view engine', 'ejs');
 
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({});
-    res.render('products/index', { products })
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category: category })
+        res.render('products/index', { products, category })
+    } else {
+        const products = await Product.find({});
+        res.render('products/index', { products, category: "All" })
+    }
+
+
+
 })
 
 app.get('/products/new', (req, res) => {
     res.render('products/new', { categories });
 })
 app.post('/products', async (req, res) => {
-    // console.log(req.body)
+
     const newProduct = new Product(req.body);
     await newProduct.save();
     // console.log(newProduct);
